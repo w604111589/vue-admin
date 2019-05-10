96d15d0723878d4ddf745182b6d0e29f93827c19
w604111589 <604111589@qq.com>
2019-05-10 20:51:12 +0800

    666

--- src/router/index.ts
+++ src/router/index.ts
@@ -54,24 +54,14 @@ export default new Router({
     },
     {
       path: '/course',
-<<<<<<< HEAD
-      name: 'home',
-      component: (resolve) => require(['@/views/front/article/index.vue'], resolve),
-=======
       name: 'course',
       component: () => import('@/views/front/article/index.vue'),
->>>>>>> 666
       meta: { hidden: true },
     },
     {
       path: '/course/detail',
-<<<<<<< HEAD
-      name: 'home',
-      component: (resolve) => require(['@/views/front/article/detail.vue'], resolve),
-=======
       name: 'courseDetail',
       component: () => import('@/views/front/article/detail.vue'),
->>>>>>> 666
       meta: { hidden: true },
     },
     {
